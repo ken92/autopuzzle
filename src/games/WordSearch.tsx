f25ab64810fc02cce243e54cfe0fcaaf3b68b6be
ken92 <ken92@users.noreply.github.com>
2025-10-05 18:26:17 +0000

    deploy: 084ccdeb1a1e95774e2f86e86122c2ff182e0e1e

--- src/games/WordSearch.tsx
+++
@@ -1,7 +0,0 @@
-import type { GameProps } from "../lib/types";
-
-const WordSearch = ({ difficulty, showSolution }: GameProps) => {
-  return <div>Word Search Game</div>;
-};
-
-export default WordSearch;
--- src/lib/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type GameDifficulty = 'easy' | 'medium' | 'hard' | 'very-hard' | 'insane' | 'inhuman';
-
-export interface GameProps {
-  difficulty: GameDifficulty;
-  showSolution: boolean;
-}
